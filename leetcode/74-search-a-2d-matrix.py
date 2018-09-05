db11e43af7420aa7bbf38db3a6bb0ced300aa15f
Joshua Goller <joshua.goller@uber.com>
2018-09-04 22:26:46 -0700

    74-search-a-2d-matrix.py

--- leetcode/74-search-a-2d-matrix.py
+++ leetcode/74-search-a-2d-matrix.py
@@ -91,3 +91,12 @@ class Solution:
         index = binary_search(target, flat_matrix)
 
         return index >= 0
+
+
+if __name__ == '__main__':
+    s = Solution()
+    assert s.searchMatrix([
+        [1,   3,  5,  7],
+        [10, 11, 16, 20],
+        [23, 30, 34, 50]
+    ], 3) == True
