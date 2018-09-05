5aae4f5d8dd5953fcdf5dca138d122178ff7c3ff
Joshua Goller <joshua.goller@uber.com>
2018-09-04 19:34:13 -0700

    clean up, working top-down

--- leetcode/decode-numstring-constant.py
+++ leetcode/decode-numstring-constant.py
@@ -166,30 +166,28 @@ class Solution:
 
     def numDecodings(self, string):
         """
-        Iterative, constant-time solution (top-down). Starting instead with string[1], if string[i-1] is
-        '1' or '2' and string[i] is not '0', we count it as a valid 2-decoding. If string[i] isn't '0',
-        it counts as a valid 1 decoding. Then, just perform string[i] = string[i-1] + string[i-2].
+        Iterative, constant-time solution (top-down).
 
-        Ex: 1223
-            1
-             2
-              3
-               5
-
-        Adopted from: https://leetcode.com/problems/decode-ways/discuss/30384/A-concise-dp-solution
+        Adapted from: https://leetcode.com/problems/decode-ways/discuss/30384/A-concise-dp-solution
         """
+        # If the string is empty or begins with a '0', it has no valid decoding.
         if not string or string[0] == '0':
             return 0
 
         k_1 = 1
         k_2 = 1
-        for i, char in enumerate(string[1:], 1):
+        for i in range(1, len(string)):
+            # If we encounter a zero, there are no valid one-character decodings at this position.
             if string[i] == '0':
                 k_1 = 0
 
+            # If we can decode a value between 10 and 26, there's also a valid two character decoding. So,
+            # the current character has k_1 + k_2 decodings, and the old k_1 becomes k_2
             if string[i - 1] == '1' or string[i-1] == '2' and 0 <= int(string[i]) <= 6:
                 k_1 = k_2 + k_1
                 k_2 = k_1 - k_2
+
+            # There's no valid two character decoding (i.e. '40'), so the old k_1 becomes k_2.
             else:
                 k_2 = k_1
 
