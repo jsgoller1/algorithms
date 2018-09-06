17b854da3314985436816e16185ec4ac9f2fb7cf
Joshua Goller <joshua.goller@uber.com>
2018-09-06 01:10:39 -0700

    more notes on 122

--- leetcode/122-best-time-to-buy-and-sell-stock-ii.py
+++ leetcode/122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,23 +1,33 @@
 """
-Statement: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+Statement: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description/
 
 Say you have an array for which the ith element is the price of a given stock on day i.
 
-If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+Design an algorithm to find the maximum profit. You may complete as many transactions
+as you like (i.e., buy one and sell one share of the stock multiple times).
 
-Note that you cannot sell a stock before you buy one.
+Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
 
 Example 1:
 
 Input: [7,1,5,3,6,4]
-Output: 5
-Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
-             Not 7-1 = 6, as selling price needs to be larger than buying price.
+Output: 7
+Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+             Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
 Example 2:
+
+Input: [1,2,3,4,5]
+Output: 4
+Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+             Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are
+             engaging multiple transactions at the same time. You must sell before buying again.
+Example 3:
 
 Input: [7,6,4,3,1]
 Output: 0
 Explanation: In this case, no transaction is done, i.e. max profit = 0.
+on is done, i.e. max profit = 0.
+
 ----
 Understand
 
@@ -83,8 +93,7 @@ array where we sell before the last day, and we add another element.
           -> [2,4,3,4,2] no backtracking
 
 What about thinking about it from the perspective of the n, n-1, and n-2 days (since we have 3 possible states)?
-  n-1, n
-
+    - Asking this led me to the pseudocode in the Plan section.
 
 ----
 Plan
@@ -113,8 +122,12 @@ if last item is B, swap to H
 
 ----
 Execute
+
+See below
 ----
 Review
+
+O(n) for time, O(c) for space, beats 60% of other submissions.
 """
 
 
@@ -137,12 +150,10 @@ class Solution:
                 else:
                     total -= prices[i - 1]
                     total += prices[i]
-                    prev = 's'  # TODO: unnecessary
             else:
                 if prices[i - 1] > prices[i]:
                     total += prices[i - 1]
                     total -= prices[i]
-                    prev = 'b'  # TODO: unnecessary
                 else:
                     total += prices[i]
                     prev = 's'
