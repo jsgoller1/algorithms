b7a20da8d2e92efdbdfb07767309a9eab4cb3043
Joshua Goller <joshua.goller@uber.com>
2018-10-09 17:49:59 -0700

    working on stripped 151

--- leetcode/general/151-reverse-all-words-in-a-string.c
+++ leetcode/general/151-reverse-all-words-in-a-string.c
@@ -22,7 +22,7 @@ output: string, modified correctly so it is reversed and has no superfluous spac
 
 
 Compile with:
-clang -std=c11 -Weverything -Werror 151-reverse-all-words-in-a-string.c -o ../../bin/151.bin && ../../bin/151.bin
+clang -std=c11 -g -Weverything -Werror 151-reverse-all-words-in-a-string.c -o ../../bin/151.bin && ../../bin/151.bin
 ----
 Understand / Plan
 
@@ -54,17 +54,66 @@ Viewed the following discussions:
 #include <stdio.h>
 #include <string.h>
 
-static size_t strip_leading_spaces(char *const s, size_t len)
+static size_t left_shift_string(char *s, size_t len, size_t dst_i, size_t src_i)
 {
-  (void)s;
-  (void)len;
-  return 0;
+  /*
+  Given string s of length len, move the substring starting at index src_i to
+  index dst_i, destructively overwriting a portion of the string. Appropriately moves
+  (assumed-present) the null terminator, and returns the size of the shrunken string.
+  Returns -1 on error.
+  */
+
+  if (src_i < dst_i)
+  {
+    printf("left_shift_string() | illegal src (%lu) and dst (%lu) indices; src must be greater than dst.\n", src_i, dst_i);
+    return len;
+  }
+
+  if (!(((0 < src_i) && (src_i < len)) && ((0 <= dst_i) && (dst_i < len))))
+  {
+    printf("left_shift_string() | given src (%lu) and dst (%lu) would violate string boundaries.\n", src_i, dst_i);
+    return len;
+  }
+
+  char *src_substr = s + (src_i * sizeof(char));
+  char *dst_substr = s + (dst_i * sizeof(char));
+
+  while (*src_substr != '\0')
+  {
+    *dst_substr = *src_substr;
+    src_substr++;
+    dst_substr++;
+  }
+  *dst_substr = '\0';
+
+  return len - (src_i - dst_i);
+}
+
+static size_t strip_leading_spaces(char *s, size_t len)
+{
+  size_t true_start = 0;
+  size_t i = 0;
+  while (*(s + i) == ' ')
+  {
+    true_start++;
+    i++;
+  }
+
+  size_t new_len = left_shift_string(s, len, 0, true_start);
+  return new_len;
 }
 
 static void reverse(char *const s, const size_t len)
 {
-  (void)s;
-  (void)len;
+  size_t i = 0;
+  size_t j = len - 1;
+  char tmp = '\0';
+  while (i++ < j--)
+  {
+    tmp = s[i];
+    s[i] = s[j];
+    s[j] = tmp;
+  }
 }
 
 static void reverse_words(char *const s, const size_t len)
@@ -76,49 +125,42 @@ static void reverse_words(char *const s, const size_t len)
 static size_t strip_middle_spaces(char *const s, size_t len)
 {
   (void)s;
-  (void)len;
-  return 0;
+  return len;
 }
 
 static size_t strip_trailing_spaces(char *const s, size_t len)
 {
-  (void)s;
-  (void)len;
-  return 0;
+  while (*(s + len - 1) == ' ')
+  {
+    len--;
+  }
+  *(s + len) = '\0';
+  return len;
 }
 
 static void reverseWords(char *s)
 {
   size_t len = strlen(s);
   len = strip_leading_spaces(s, len);
+  len = strip_middle_spaces(s, len);
+  len = strip_trailing_spaces(s, len);
+  printf("Stripped: %s.\n", s);
   reverse(s, len);
   reverse_words(s, len);
-  len = strip_middle_spaces(s, len);
-  (void)strip_trailing_spaces(s, len);
 }
 
 int main()
 {
   // basic smoke test
-  char string1[] = "the sky is blue";
+  char string1[] = "    the     sky     is    blue   ";
   char string2[] = "blue is sky the";
   reverseWords(string1);
   if (strcmp(string1, string2) != 0)
   {
-    printf("Failed to first second string: %s\n", string1);
+    printf("Got: %s.\nExpected: %s.\n", string1, string2);
     return -1;
   }
 
-  // Test leading and middle spaces
-  char string3[] = "   the     sky  is   blue   ";
-  char string4[] = "blue is sky the";
-  reverseWords(string3);
-  if (strcmp(string3, string4) != 0)
-  {
-    printf("Failed to reverse second string: %s\n", string3);
-    return -1;
-  }
-
-  printf("Correctly reversed string.\n");
+  printf("Correctly reversed strings.\n");
   return 0;
 }
