a26eef6d48dddb66ca3d7507476ac285f1359cc8
Joshua Goller <joshua.goller@uber.com>
2018-11-01 03:15:24 -0700

    working lc 583

--- leetcode/dp/583-delete-operation-for-two-strings.py
+++ leetcode/dp/583-delete-operation-for-two-strings.py
@@ -35,34 +35,26 @@ lcs(word1, word2):
 minDelete(word1, word2):
   return 2*lcs(word1, word2)-len(word1)-len(word2)
 """
+
+
 class Solution:
-  def __init__(self):
-    self.cache = {}
+    def minDistance(self, word1, word2):
+        lcs = [[0 for col in range(len(word2)+1)]
+               for row in range(len(word1) + 1)]
 
-  def lcs(self, word1, word2):
-    if word1 == "" or word2 == "":
-      return 0
+        for row, char1 in enumerate(word1, 1):
+            for col, char2 in enumerate(word2, 1):
+                if char1 == char2:
+                    lcs[row][col] = lcs[row - 1][col - 1] + 1
+                else:
+                    lcs[row][col] = max(
+                        lcs[row - 1][col], lcs[row][col - 1])
+        return len(word1)+len(word2)-2*lcs[-1][-1]
 
-    if word1 < word2:
-      pair = (word1,word2)
-    else:
-      pair = (word2,word1)
-    if pair in self.cache:
-      return self.cache[pair]
-
-    if word1[-1] == word2[-1]:
-      self.cache[pair] = self.lcs(word1[:-1], word2[:-1]) + 1
-    else:
-      self.cache[pair] = max(self.lcs(word1, word2[:-1]), self.lcs(word1[:-1], word2))
-    return self.cache[pair]
-
-  def minDistance(self, word1, word2):
-    self.cache = {}
-    return len(word1)+len(word2)-(2*self.lcs(word1, word2))
 
 if __name__ == '__main__':
-  s = Solution()
-  assert s.lcs("cat", "bad") == 1
-  assert s.minDelete("cat", "bad") == 4
-  assert s.lcs("", "") == 0
-  assert s.lcs("ccaccjp", "fwosarcwge") == 2
+    s = Solution()
+    assert s.minDistance("", "") == 0
+    assert s.minDistance("a", "ab") == 1
+    assert s.minDistance("cat", "bad") == 4
+    assert s.minDistance("sea", "eat") == 2
