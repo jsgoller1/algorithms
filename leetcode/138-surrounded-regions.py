7642c24acd90b4d89caec86104d2006c83fbf864
Joshua Goller <joshua.goller@uber.com>
2018-11-13 02:44:36 -0800

    working on LC130

---
+++ leetcode/138-surrounded-regions.py
@@ -0,0 +1,133 @@
+"""
+Given a 2D board containing 'X' and 'O' (the letter O), capture
+all regions surrounded by 'X'.
+
+A region is captured by flipping all 'O's
+into 'X's in that surrounded region.
+
+Example:
+
+X X X X
+X O O X
+X X O X
+X O X X
+
+After running your function, the board should be:
+X X X X
+X X X X
+X X X X
+X O X X
+
+Explanation:
+Surrounded regions shouldn’t be on the border, which means
+that any 'O' on the border of the board are not flipped to
+'X'. Any 'O' that is not on the border and it is not connected
+to an 'O' on the border will be flipped to 'X'. Two cells are
+connected if they are adjacent cells connected horizontally
+or vertically.
+-----------------------
+Input: List[List[str]]
+Output: None, modify input in-place
+
+- DFS solution:
+  - go through each cell of the board, mantaining visited set
+  - if we find an 0, begin a dfs on that cell
+    - dfs uses visited set, possible Os and looks for other O cells
+    - if the dfs finds a border, none of the Os can be converted as
+    they are connected to an unsurrounded and by extension none of them are surrounded
+    - if no border is found and the dfs is exhausted, convert all Os to Xs
+    - using the visited set with the DFS will ensure no cells are visited twice
+"""
+
+
+class Solution(object):
+    def surroundDFS(self, y, x, visited, board):
+        toCapture = set()
+        q = [(y, x)]
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        while q:
+            currY, currX = q.pop()
+            #print("Visiting {0}".format((currY, currX)))
+            if not ((1 <= currY < len(board) - 1) and (1 <= currX < len(board[0]) - 1)):
+                return
+            visited.add((currY, currX))
+            toCapture.add((currY, currX))
+            for direction in directions:
+                nextY = currY + direction[0]
+                nextX = currX + direction[1]
+                if board[nextY][nextX] == 'O' and (nextY, nextX) not in visited:
+                    q.append((nextY, nextX))
+        for coords in toCapture:
+            y, x = coords
+            board[y][x] = 'X'
+
+    def solve(self, board):
+        visited = set()
+        for y, row in enumerate(board):
+            for x, col in enumerate(row):
+                if (y, x) not in visited and board[y][x] == "O":
+                    self.surroundDFS(y, x, visited, board)
+                else:
+                    visited.add((y, x))
+
+
+if __name__ == '__main__':
+    s = Solution()
+    tests = [([["X", "X", "X", "X"],
+               ["X", "O", "O", "X"],
+               ["X", "X", "O", "X"],
+               ["X", "O", "X", "X"]], [["X", "X", "X", "X"],
+                                       ["X", "X", "X", "X"],
+                                       ["X", "X", "X", "X"],
+                                       ["X", "O", "X", "X"]]),
+
+             ([["X", "X", "X", "X"],
+               ["X", "X", "X", "X"],
+                 ["X", "X", "X", "X"],
+                 ["X", "X", "X", "X"]], [["X", "X", "X", "X"],
+                                         ["X", "X", "X", "X"],
+                                         ["X", "X", "X", "X"],
+                                         ["X", "X", "X", "X"]]),
+             ([["O", "O", "O", "O"],
+               ["O", "O", "O", "O"],
+                 ["O", "O", "O", "O"],
+                 ["O", "O", "O", "O"]], [["O", "O", "O", "O"],
+                                         ["O", "O", "O", "O"],
+                                         ["O", "O", "O", "O"],
+                                         ["O", "O", "O", "O"]]),
+             ([[]], [[]]),
+             ([["X", "O", "O", "X", "X", "X", "O", "X", "O", "O"],
+               ["X", "O", "X", "X", "X", "X", "X", "X", "X", "X"],
+                 ["X", "X", "X", "X", "O", "X", "X", "X", "X", "X"],
+                 ["X", "O", "X", "X", "X", "O", "X", "X", "X", "O"],
+                 ["O", "X", "X", "X", "O", "X", "O", "X", "O", "X"],
+                 ["X", "X", "O", "X", "X", "O", "O", "X", "X", "X"],
+                 ["O", "X", "X", "O", "O", "X", "O", "X", "X", "O"],
+                 ["O", "X", "X", "X", "X", "X", "O", "X", "X", "X"],
+                 ["X", "O", "O", "X", "X", "O", "X", "X", "O", "O"],
+                 ["X", "X", "X", "O", "O", "X", "O", "X", "X", "O"]], [["X", "O", "O", "X", "X", "X", "O", "X", "O", "O"],
+                                                                       ["X", "O", "X", "X", "X",
+                                                                        "X", "X", "X", "X", "X"],
+                                                                       ["X", "X", "X", "X", "X",
+                                                                        "X", "X", "X", "X", "X"],
+                                                                       ["X", "X", "X", "X", "X",
+                                                                        "X", "X", "X", "X", "O"],
+                                                                       ["O", "X", "X", "X", "X",
+                                                                        "X", "X", "X", "X", "X"],
+                                                                       ["X", "X", "X", "X", "X",
+                                                                        "X", "X", "X", "X", "X"],
+                                                                       ["O", "X", "X", "X", "X",
+                                                                        "X", "X", "X", "X", "O"],
+                                                                       ["O", "X", "X", "X", "X",
+                                                                        "X", "X", "X", "X", "X"],
+                                                                       ["X", "X", "X", "X", "X",
+                                                                        "X", "X", "X", "O", "O"],
+                                                                       ["X", "X", "X", "O", "O", "X", "O", "X", "X", "O"]])]
+
+    for test in tests:
+        board = test[0]
+        solution = test[1]
+        s.solve(board)
+        for y, row in enumerate(board):
+            for x, col in enumerate(row):
+                assert col == solution[y][x]
