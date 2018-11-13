b87bdaf01bd52172921305b7e6876721bc3bcf08
Joshua Goller <joshua.goller@uber.com>
2018-11-13 04:58:15 -0800

    update LC130

--- leetcode/130-surrounded-regions.py
+++ leetcode/130-surrounded-regions.py
@@ -33,7 +33,7 @@ Output: None, modify input in-place
   - go through each cell of the board, mantaining visited set
   - if we find an 0, begin a dfs on that cell
     - dfs uses visited set, possible Os and looks for other O cells
-    - if the dfs finds a border, none of the Os can be converted as
+    - if the dfs finds a border or cell known to be uncapturable, none of the Os can be converted as
     they are connected to an unsurrounded and by extension none of them are surrounded
     - if no border is found and the dfs is exhausted, convert all Os to Xs
     - using the visited set with the DFS will ensure no cells are visited twice
@@ -41,34 +41,44 @@ Output: None, modify input in-place
 
 
 class Solution(object):
-    def surroundDFS(self, y, x, visited, board):
-        toCapture = set()
+    def surroundDFS(self, y, x, visited, uncapturable, board):
+        discovered = set()
+        capturable = True
         q = [(y, x)]
         directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         while q:
             currY, currX = q.pop()
             #print("Visiting {0}".format((currY, currX)))
-            if not ((1 <= currY < len(board) - 1) and (1 <= currX < len(board[0]) - 1)):
-                return
             visited.add((currY, currX))
-            toCapture.add((currY, currX))
+            discovered.add((currY, currX))
+            if not ((1 <= currY < len(board) - 1) and (1 <= currX < len(board[0]) - 1)):
+                capturable = False
+                continue
             for direction in directions:
                 nextY = currY + direction[0]
                 nextX = currX + direction[1]
-                if board[nextY][nextX] == 'O' and (nextY, nextX) not in visited:
-                    q.append((nextY, nextX))
-        for coords in toCapture:
-            y, x = coords
-            board[y][x] = 'X'
+                if board[nextY][nextX] == 'O':
+                    if (nextY, nextX) in uncapturable:
+                        capturable = False
+                    elif (nextY, nextX) not in visited:
+                        q.append((nextY, nextX))
+
+        for y, x in discovered:
+            if capturable:
+                board[y][x] = 'X'
+            else:
+                uncapturable.add((y, x))
 
     def solve(self, board):
         visited = set()
+        uncapturable = set()
         for y, row in enumerate(board):
             for x, col in enumerate(row):
-                if (y, x) not in visited and board[y][x] == "O":
-                    self.surroundDFS(y, x, visited, board)
-                else:
-                    visited.add((y, x))
+                if board[y][x] not in visited:
+                    if col == "O":
+                        self.surroundDFS(y, x, visited, uncapturable, board)
+                    else:
+                        visited.add((y, x))
 
 
 if __name__ == '__main__':
@@ -116,7 +126,27 @@ if __name__ == '__main__':
                   ["O", "X", "X", "X", "X", "X", "X", "X", "X", "O"],
                   ["O", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
                   ["X", "X", "X", "X", "X", "X", "X", "X", "O", "O"],
-                  ["X", "X", "X", "O", "O", "X", "O", "X", "X", "O"]])]
+                  ["X", "X", "X", "O", "O", "X", "O", "X", "X", "O"]]),
+             ([["O", "X", "O", "O", "O", "O", "O", "O", "O"],
+               ["O", "O", "O", "X", "O", "O", "O", "O", "X"],
+               ["O", "X", "O", "X", "O", "O", "O", "O", "X"],
+               ["O", "O", "O", "O", "X", "O", "O", "O", "O"],
+               ["X", "O", "O", "O", "O", "O", "O", "O", "X"],
+               ["X", "X", "O", "O", "X", "O", "X", "O", "X"],
+               ["O", "O", "O", "X", "O", "O", "O", "O", "O"],
+               ["O", "O", "O", "X", "O", "O", "O", "O", "O"],
+               ["O", "O", "O", "O", "O", "X", "X", "O", "O"]],
+              #
+              [["O", "X", "O", "O", "O", "O", "O", "O", "O"],
+               ["O", "O", "O", "X", "O", "O", "O", "O", "X"],
+               ["O", "X", "O", "X", "O", "O", "O", "O", "X"],
+               ["O", "O", "O", "O", "X", "O", "O", "O", "O"],
+               ["X", "O", "O", "O", "O", "O", "O", "O", "X"],
+               ["X", "X", "O", "O", "X", "O", "X", "O", "X"],
+               ["O", "O", "O", "X", "O", "O", "O", "O", "O"],
+               ["O", "O", "O", "X", "O", "O", "O", "O", "O"],
+               ["O", "O", "O", "O", "O", "X", "X", "O", "O"]]
+              )]
 
     for test in tests:
         board = test[0]
@@ -124,4 +154,10 @@ if __name__ == '__main__':
         s.solve(board)
         for y, row in enumerate(board):
             for x, col in enumerate(row):
-                assert col == solution[y][x]
+                print("Verifying {0}".format((y, x)))
+                try:
+                    assert col == solution[y][x]
+                except AssertionError:
+                    for row in board:
+                        print(row)
+                    raise AssertionError
