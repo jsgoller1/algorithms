ae597121cf671ad207eaed9d37a1b80ab518240a
Joshua Goller <joshua.goller@uber.com>
2018-12-13 22:54:00 -0800

    update failures

--- failures/randomized-backtracking-sudoku-solver.py
+++ failures/randomized-backtracking-sudoku-solver.py
@@ -4,62 +4,70 @@ I thought I could be clever and implement a randomized search like Skiena demons
 in the Algorithm Design Manual to circumvent pathological sudoku boards, but it fails to work for
 hard cases (I started the program like 10 minutes ago and it still isn't finished). There are
 solutions to all LeetCode cases, so I should rethink my approach.
+--------------
+After discovering that I was unnecessarily validating the board, I came back and optimized the validation function to only
+check a cell's row / col / square, but this didn't do any better.
 """
 import random
 import collections
 
 
 class Solution:
-    def valid(self, board):
-        # rows
-        for row in board:
-            row_set = set()
-            for item in row:
-                if item in row_set:
-                    return False
-                elif item != '.':
-                    row_set.add(item)
+    def validCell(self, board, cellId):
+        row, col = cellId // 9, cellId % 9
 
-        # columns
-        for i in range(9):
-            col_set = set()
-            for row in board:
-                if row[i] in col_set:
-                    return False
-                elif row[i] != '.':
-                    col_set.add(row[i])
+        # row
+        row_set = set()
+        for item in board[row]:
+            if item in row_set:
+                return False
+            elif item != '.':
+                row_set.add(item)
 
-        # 3x3s
+        # column
+        col_set = set()
+        for each_row in board:
+            if each_row[col] in col_set:
+                return False
+            elif each_row[col] != '.':
+                col_set.add(each_row[col])
+
+        # 3x3
+        y = 0
+        x = 0
         square_starts = [0, 3, 6]
-        for y in square_starts:
-            for x in square_starts:
-                square_set = set()
-                for i in range(3):
-                    for j in range(3):
-                        if board[y + i][x + j] in square_set:
-                            return False
-                        elif board[y + i][x + j] != '.':
-                            square_set.add(board[y + i][x + j])
+        for nearestY in square_starts:
+            if row > nearestY:
+                y = nearestY
+        for nearestX in square_starts:
+            if col > nearestX:
+                x = nearestX
+
+        square_set = set()
+        for i in range(3):
+            for j in range(3):
+                if board[y + i][x + j] in square_set:
+                    return False
+                elif board[y + i][x + j] != '.':
+                    square_set.add(board[y + i][x + j])
         return True
 
     def solve(self, board, searchOrder):
-        if not searchOrder and self.valid(board):
+        if not searchOrder:
             return True
 
         row, col = searchOrder[0] // 9, searchOrder[0] % 9
         temp = searchOrder.popleft()
-        if board[row][col] != '.':
-            return self.solve(board, searchOrder)
-        else:
-            for val in range(1, 10):
-                board[row][col] = str(val)
-                if self.valid(board) and self.solve(board, searchOrder):
-                    return True
-            board[row][col] = '.'
+        for val in range(1, 10):
+            board[row][col] = str(val)
+            if self.validCell(board, temp) and self.solve(board, searchOrder):
+                return True
+        board[row][col] = '.'
         searchOrder.appendleft(temp)
 
     def solveSudoku(self, board):
-        searchOrder = collections.deque([i for i in range(81)])
+        searchOrder = collections.deque(
+            [i for i in range(81) if board[i // 9][i % 9] == '.'])
         random.shuffle(searchOrder)
         self.solve(board, searchOrder)
 
@@ -130,10 +138,10 @@ if __name__ == '__main__':
         ["2", "8", "7", "4", "1", "9", "6", "3", "5"],
         ["3", "4", "5", "2", "8", "6", "1", "7", "9"]
     ]
-    s.solveSudoku(hardBoard)
-    for row in hardBoard:
+    s.solveSudoku(mediumBoard)
+    for row in mediumBoard:
         print(row)
-    assert hardBoard == solvedBoard
+    assert mediumBoard == solvedBoard
     #assert s.solveSudoku(solvedBoard) == True
     #assert s.solveSudoku(solvedBoardInvalid) == False
     #assert s.solveSudoku(validBoard) == True
