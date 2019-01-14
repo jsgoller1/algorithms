06d34b47c4e7df56d5dfe6e274f77af865dacbac
Joshua Goller <joshua.goller@uber.com>
2019-01-14 02:23:32 -0800

    add tree visualizer tool

---
+++ tools/tree_visualizer.py
@@ -0,0 +1,63 @@
+import turtle
+
+"""
+Stefan Pochmann's tree visualizer tool. See:
+https://leetcode.com/problems/recover-binary-search-tree/discuss/32539/Tree-Deserializer-and-Visualizer-for-Python
+"""
+
+
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __repr__(self):
+        return 'TreeNode({})'.format(self.val)
+
+
+def deserialize(string):
+    if string == '{}':
+        return None
+    nodes = [None if val == 'null' else TreeNode(int(val))
+             for val in string.strip('[]{}').split(',')]
+    kids = nodes[::-1]
+    root = kids.pop()
+    for node in nodes:
+        if node:
+            if kids:
+                node.left = kids.pop()
+            if kids:
+                node.right = kids.pop()
+    return root
+
+
+def drawtree(root):
+    def height(root):
+        return 1 + max(height(root.left), height(root.right)) if root else -1
+
+    def jumpto(x, y):
+        t.penup()
+        t.goto(x, y)
+        t.pendown()
+
+    def draw(node, x, y, dx):
+        if node:
+            t.goto(x, y)
+            jumpto(x, y-20)
+            t.write(node.val, align='center', font=('Arial', 12, 'normal'))
+            draw(node.left, x-dx, y-60, dx/2)
+            jumpto(x, y-20)
+            draw(node.right, x+dx, y-60, dx/2)
+    t = turtle.Turtle()
+    t.speed(0)
+    turtle.delay(0)
+    h = height(root)
+    jumpto(0, 30*h)
+    draw(root, 0, 30*h, 40*h)
+    t.hideturtle()
+    turtle.mainloop()
+
+
+if __name__ == '__main__':
+    drawtree(deserialize('[3,null,30,10,null,null,15,null,45]'))
