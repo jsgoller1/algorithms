e43074b5fdabdb01f270ea579e2621868721268a
Joshua Goller <joshua.goller@uber.com>
2019-04-07 13:11:21 -0700

    update make system; use c and cpp makefiles

--- makefiles/build.mk
+++ makefiles/build.mk
@@ -1,14 +1,27 @@
 PWD:= `pwd`
 EXERCISE:= `basename $$PWD`
 
+
+### Binary cleanup
+setup:
+	-mkdir $(BUILD_DIR)
+
+# Note: the folder is called /execs so you don't accidentally
+# run $rm -r against your system's /bin directory.
+clean:
+	-rm -r $(BUILD_DIR)/*
+
+reset:
+	reset
+
 # Duplicated because GNU Make doesn't support mixing implicit and explicit rules, *eyeroll*
 solution: reset clean setup
 	$(COMPILE) $@.cpp -o $(BUILD_DIR)/$(EXERCISE)-$@
-	$(BUILD_DIR)/$(EXERCISE)-$@
+	$(VALGRIND) $(BUILD_DIR)/$(EXERCISE)-$@
 
 alternate-%: reset clean setup
 	$(COMPILE) $@.cpp -o $(BUILD_DIR)/$(EXERCISE)-$@
-	$(BUILD_DIR)/$(EXERCISE)-$@
+	$(VALGRIND) $(BUILD_DIR)/$(EXERCISE)-$@
 
 # There may not be a Python solution, but if there is, you can $make python
 python:
--- makefiles/cpp-compile.mk
+++ makefiles/cpp-compile.mk
@@ -14,15 +14,3 @@ COMPILE:=$(ANALYZER) $(CC) $(CFLAGS) $(WARNINGS)
 
 ### Valgrind target for memory analysis
 VALGRIND := valgrind -q --leak-check=full --show-leak-kinds=all --track-origins=yes --error-exitcode=42
-
-### Binary cleanup
-setup:
-	-mkdir $(BUILD_DIR)
-
-# Note: the folder is called /execs so you don't accidentally
-# run $rm -r against your system's /bin directory.
-clean:
-	-rm -r $(BUILD_DIR)/*
-
-reset:
-	reset
