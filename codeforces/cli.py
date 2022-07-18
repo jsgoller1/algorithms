3964270ff06214cc22023e3c2bb824a4a80447b7
Joshua Goller <jsgoller1@gmail.com>
2022-07-18 16:03:02 -0700

    Update CLI to allow for Python solutions

--- codeforces/cli.py
+++ codeforces/cli.py
@@ -5,23 +5,29 @@ import subprocess
 import sys
 
 COMPILE_CMD = "g++ -static -DLOCAL -lm -s -x c++ -O2 -std=c++17 -D__USE_MINGW_ANSI_STDIO=0 -Wall"
-TEMPLATE_PATH = "tools/template.cpp"
+CPP_TEMPLATE_PATH = "templates/template.cpp"
+PY_TEMPLATE_PATH = "templates/template.py"
 
 @click.group()
 def cli():
     pass
 
 @click.command("create")
-@click.option("--tests", default=1, help="Number of empty test input files to create")
 @click.argument("problemset")
 @click.argument("letter")
-def create(problemset, letter, tests):
+@click.option("--tests", default=1, help="Number of empty test input files to create")
+@click.option("--python", default=False, help="Create a Python template instead of CPP")
+def create(problemset, letter, tests, python):
     if not os.path.exists(f"./{problemset}"):
         os.mkdir(f"./{problemset}")
         click.echo(f"Created dir ./{problemset}")
-    if not os.path.exists(f"./{problemset}/{letter}.cpp"):
-        shutil.copyfile(TEMPLATE_PATH, f"./{problemset}/{letter}.cpp")
-        click.echo(f"Created template ./{problemset}/{letter}.cpp")
+
+    template_path = PYTHON_TEMPLATE_PATH if python else CPP_TEMPLATE_PATH
+    problem_path = f"./{problemset}/{letter}.{"py" if python else "cpp"}"
+    if not os.path.exists(problem_path):
+        shutil.copyfile(template_path, problem_path)
+        click.echo(f"Created template {problem_path}")
+
     for i in range(tests):
         test_path = f"./{problemset}/{letter}_input{i}.txt"
         if not os.path.exists(test_path):
@@ -44,15 +50,16 @@ def build(problemset, letter):
 @click.command("test")
 @click.argument("problemset")
 @click.argument("letter")
+@click.option("--python", default=False, help="Look for Python executable instead of CPP")
 def test(problemset, letter):
-    cp = subprocess.run(f"ls ./{problemset} | grep '{letter}_input'", shell=True, capture_output=True, text=True)
-    test_inputs = [filename for filename in cp.stdout.split('\n') if filename]
+    exec_cmd = f"python3 ./{problemset}/{letter}.py" if python else f"/tmp/{problemset}-{letter}.out"
+    input_files = subprocess.run(f"ls ./{problemset} | grep '{letter}_input'", shell=True, capture_output=True, text=True).stdout
+    test_inputs = [filename for filename in input_files.split('\n') if filename]
     for input_file in test_inputs:
-            cmd = f"cat ./{problemset}/{input_file} | /tmp/{problemset}-{letter}.out"
+            cmd = f"cat ./{problemset}/{input_file} | {exec_cmd}"
             click.echo(f"{cmd}")
             subprocess.run(cmd, shell=True)
             print(" ")
-
 
 if __name__ == '__main__':
     cli.add_command(create)
