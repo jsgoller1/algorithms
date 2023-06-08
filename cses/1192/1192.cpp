9de9b34e70d627f79c70552bab795178131ff446
Joshua Goller <jsgoller1@gmail.com>
2023-06-08 16:05:12 -0400

    CSES 1192

---
+++ cses/1192/1192.cpp
@@ -0,0 +1,127 @@
+// Author: Joshua Goller
+// Email: joshua.goller@hey.com
+// Website: https://jsgoller1.github.io
+
+// Includes all standard headers (no need for <vector>, <list>, etc).
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// --- I/O ---
+/*
+sync_with_stdio(): do not sync C++ streams (e.g. std::cin) with C streams
+(e.g. stdin) after each output; adds unnecessary time.
+
+tie(): untie cin from cout (tied by default); if tied, each read from cin
+flushes cout, adds unnecessary time. Unclear from Stroustrup if both need to be
+untied, doing it to be safe.
+*/
+#define sanic_io()                  \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(NULL);                    \
+  cout.tie(NULL);
+#define endl "\n"  // std::endl causes a flush, adds unnecessary time
+#define read_from_files(in_path, out_path) \
+  freopen(in_path, "r", stdin);            \
+  freopen(out_path, "w", stdout);
+#define output(val) cout << val << endl;
+#define var_in(type, var) \
+  type var;               \
+  cin >> var;
+#define iin(var) var_in(int, var)
+#define lin(var) var_in(ll, var)
+#define strin(var) var_in(string, var)
+#define chin(var) var_in(char, var)
+
+// --- Abbreviations --
+#define rep(i, n) for (ll i = 0; i < (n); i++)
+#define rep1(i, n) for (ll i = 1; i <= (n); i++)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()  // reverse iterator
+#define fi first
+#define se second
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+
+// -- Types --
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned uint;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+
+// -- Testing --
+#ifdef LOCAL
+#define eprintf(...)              \
+  {                               \
+    fprintf(stderr, __VA_ARGS__); \
+    fflush(stderr);               \
+  }
+
+#else
+#define eprintf(...) 0
+#endif
+
+clock_t start_time, case_time;
+double getCurrentTime() { return ((double)clock()) / CLOCKS_PER_SEC; }
+
+#define WALL_CHAR '#'
+#define FLOOR_CHAR '.'
+#define MARK_CHAR 'X'
+
+void bfs(vector<vector<char>>& maze, const size_t y, const size_t x) {
+  if (!(0 <= y && y < maze.size()) || !(0 <= x && x < maze[0].size())) {
+    return;
+  }
+  if (maze[y][x] != FLOOR_CHAR) {
+    return;
+  }
+  maze[y][x] = MARK_CHAR;
+  bfs(maze, y + 1, x);
+  bfs(maze, y - 1, x);
+  bfs(maze, y, x + 1);
+  bfs(maze, y, x - 1);
+}
+
+int main() {
+  sanic_io();
+  // cout << setprecision(12);
+  // read_from_files();
+  lin(height);
+  lin(width);
+  vector<vector<char>> maze;
+
+  rep(i, height) {
+    vector<char> row;
+    rep(j, width) {
+      chin(c);
+      // printf("Read in: %c\n", c);
+      row.pb(c);
+    }
+    maze.pb(row);
+  }
+
+  // From each cell, launch bfs if cell is `.`; overwrite reachable `.` with
+  // `X`.
+  // Each time new component found and BFS launched, inc count by 1.
+  // output count.
+
+  ll segmentCount = 0;
+  for (size_t y = 0; y < size_t(height); y++) {
+    for (size_t x = 0; x < size_t(width); x++) {
+      // printf("(%lu, %lu): %c\n", y, x, maze[y][x]);
+      if (maze[y][x] == FLOOR_CHAR) {
+        bfs(maze, y, x);
+        segmentCount++;
+      }
+    }
+  }
+  output(segmentCount);
+  return 0;
+}
