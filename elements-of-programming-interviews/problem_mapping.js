d66a93c7b3061e8a783c4e7215d891d9acb16b1d
Joshua Goller <jsgoller1@gmail.com>
2023-08-18 00:16:26 -0400

    8/17/23 - slow day
    I think I'm going to switch to focusing on just Python again;
    feeling like there's too much room for embarassing error when jumping
    between them.
    
    - convert_base.py

--- elements-of-programming-interviews/problem_mapping.js
+++ elements-of-programming-interviews/problem_mapping.js
@@ -504,7 +504,7 @@ problem_mapping = {
                 "total": 20001
             },
             "Python: convert_base.py": {
-                "passed": 0,
+                "passed": 20001,
                 "total": 20001
             }
         },
