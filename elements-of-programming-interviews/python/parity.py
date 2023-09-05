fb71b79a9605d1672faed03d696d83c1e4ef4748
Joshua Goller <jsgoller1@gmail.com>
2023-09-05 14:15:22 -0400

    Adds collective parity calculator for parity.py

--- elements-of-programming-interviews/python/parity.py
+++ elements-of-programming-interviews/python/parity.py
@@ -1,4 +1,5 @@
 from test_framework import generic_test
+from typing import List
 
 
 def parity(x: int) -> int:
@@ -8,6 +9,13 @@ def parity(x: int) -> int:
             parity = not parity
         x >>= 1
     return parity
+
+
+def collective_parity(vals: List[int]) -> int:
+    collected = vals[0]
+    for val in vals[1:]:
+        collected ^= val
+    return parity(collected)
 
 
 if __name__ == '__main__':
