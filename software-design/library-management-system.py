72cac4d43b3b95c3f33c3ca57fecc053dbacc8de
Joshua Goller <jsgoller1@gmail.com>
2023-10-03 12:45:30 -0400

    Adds software_design/ and partially completed first exercise

---
+++ software-design/library-management-system.py
@@ -0,0 +1,30 @@
+"""
+Design a Library Management System.
+
+Consider a system where:
+- Users can borrow books.
+- Users can return books.
+- Users can search for books by title, author, or ISBN.
+- Books have attributes like title, author, ISBN, and status (e.g., available, borrowed).
+- Users have attributes like user ID, name, and a list of books they've borrowed.
+- There should be a limit to the number of books a user can borrow at one time.
+- The system should keep track of the due date for borrowed books and can flag books that are overdue.
+- Users should be able to reserve books that are currently borrowed.
+
+Your focus should be on designing a set of extensible classes and interfaces that can model this system, 
+ensuring good OO design principles. Stub out methods where necessary. Once you have a rough design in mind,
+share it and we can go through it together.
+---------------------------------------------
+Components: 
+    - Books 
+        - Title
+        - Author
+        - ISBN
+        - Status (available, borrowed)
+    - Users 
+        - ID
+        - Name
+        - Books they've borrowed 
+
+
+"""
